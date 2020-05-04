772e64c0a8e751ce89b088c858fc63566cc23135
Julian Petersen <juli11c0@edu.ucl.dk>
2020-05-04 13:14:35 +0200

    deleted all dependecies again

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
+// import socketio from 'socket.io';
+// import VueSocketIO from 'vue-socket.io';
+
+// export const SocketInstance = socketio('http://localhost:8080');
+
+// Vue.use(VueSocketIO, SocketInstance)
+
 Vue.config.productionTip = false
 
 new Vue({
